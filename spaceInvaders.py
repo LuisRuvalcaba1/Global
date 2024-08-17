8cf857eb4c7764917562c3a4427495fc57ec2272
Luis <a20100360@ceti.mx>
2024-08-16 18:45:45 -0600

    Globlal 2.0

--- spaceInvaders.py
+++ spaceInvaders.py
@@ -1,64 +1,48 @@
 import os
 import time
 
-#Alumno : Luis Alejandro Ruvalcaba Martinez
-#Numero de registro: 24310198
+filas = 10
+columnas = 50
+naves = 5
 
-def borrarPantalla():
-    os.system("cls" if os.name == "nt" else "clear")
-
-def imprimirTablero(tablero):
-    for renglon in tablero:
-        print("".join(renglon))
-
-def inicializarTablero(filas, columnas, naves):
-    tablero = [[" " for _ in range(columnas)] for _ in range(filas)]
-    for i in range(naves):
-        tablero[0][i * 5:(i * 5) + 5] = " * * "
-        tablero[1][i * 5:(i * 5) + 5] = "  *  "
+def tableroMain(filas, columnas):
+    tablero = []
+    for i in range(filas):
+        fila = [" "] * columnas
+        tablero.append(fila)
     return tablero
 
-def moverNaves(tablero, direccion):
-    filas = len(tablero)
-    columnas = len(tablero[0])
-    borde_izquierdo, borde_derecho = False, False
-
+def iniciarTableroNaves(filas, columnas, naves):
+    tablero = []
     for i in range(filas):
+        fila = [" "] * columnas
+        tablero.append(fila)
+
+    for i in range(naves):
+        for j in range(5):
+            tablero[0][i * 5 + j] = " * * "[j]
+        for j in range(5):
+            tablero[1][i * 5 + j] = "  *  "[j]
+
+    return tablero
+
+def imprimirtablero(tablero):
+    for fila in tablero:
+        print("".join(fila))
+
+def mover_abajo(tablero, filas, columnas):
+    for i in range(filas - 1, 0, -1):
         for j in range(columnas):
-            if tablero[i][j] != " ":
-                if direccion == 1 and j >= columnas - 5: 
-                    borde_derecho = True
-                if direccion == -1 and j <= 4:
-                    borde_izquierdo = True
+            tablero[i][j] = tablero[i-1][j]
+    for j in range(columnas):
+        tablero[0][j] = " "
+    return tablero
 
-    if borde_derecho or borde_izquierdo:
-        direccion *= -1
-        tablero = [[" " for _ in range(columnas)]] + tablero[:-1] + [[" " for _ in range(columnas)]]
+tablero = iniciarTableroNaves(filas, columnas, naves)
 
-    nuevo_tablero = [[" " for _ in range(columnas)] for _ in range(filas)]
-    for i in range(filas):
-        for j in range(columnas):
-            if tablero[i][j] != " ":
-                if tablero[i][j] == "*":
-                    nuevo_tablero[i][j + direccion] = "*"
-                else:
-                    nuevo_tablero[i][j + direccion] = tablero[i][j]
-
-    return nuevo_tablero, direccion
-
-def main():
-    filas = 10
-    columnas = 60
-    naves = 10
-    tablero = inicializarTablero(filas, columnas, naves)
-    direccion = 1  # 1 para derecha, -1 para izquierda
-
-    while True:
-        borrarPantalla()
-        imprimirTablero(tablero)
-        tablero, direccion = moverNaves(tablero, direccion)
-        time.sleep(0.2)
-
-if __name__ == "__main__":
-    main()
-
+# Mover las naves hacia abajo
+for _ in range(8):
+    os.system('cls' if os.name == 'nt' else 'clear') 
+    imprimirtablero(tablero)
+    tablero = mover_abajo(tablero, filas, columnas)
+    time.sleep(1)
