458bc1f0479b0c408f5a373a72736b96529c2d3b
Luis <a20100360@ceti.mx>
2024-08-23 11:09:07 -0600

    Bajan pero no se elimina el anterior y no se toma la base impresa anteriormente

--- spaceinvaders.c
+++ spaceinvaders.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include <stdlib.h>
 #include <conio.h>
 
-char iniciar_tablero(char tablero[10][60])
-{
+#define columnas 60
+#define filas 10
 
-    for (int i = 0; i < 10; i++)
+char iniciar_tablero(char tablero[filas][columnas])
+{
+    for (int i = 0; i < filas; i++)
     {
-        for (int j = 0; j < 60; j++)
+        for (int j = 0; j < columnas; j++)
         {
             tablero[i][j] = ' ';
         }
     }
 }
 
-void imprimir_tablero(char tablero[10][60])
+void imprimir_tablero(char tablero[filas][columnas])
 {
     for (int i = 0; i < 10; i++)
     {
@@ -25,52 +30,56 @@ void imprimir_tablero(char tablero[10][60])
     }
 }
 
-char iniciar_naves(char tablero[10][60], int naves)
+char iniciar_naves(char tablero[filas][columnas], int naves)
 {
     for (int i = 0; i < naves; i++)
     {
         for (int j = 0; j < 5; j++)
         {
             tablero[0][i * 5] = ' ';
-            tablero[0][(i * 5 )+ 1] = '*';
-            tablero[0][(i * 5 )+ 2] = ' ';
-            tablero[0][(i * 5 )+ 3] = '*';
-            tablero[0][(i * 5 )+ 4] = ' ';
+            tablero[0][(i * 5) + 1] = '*';
+            tablero[0][(i * 5) + 2] = ' ';
+            tablero[0][(i * 5) + 3] = '*';
+            tablero[0][(i * 5) + 4] = ' ';
 
             tablero[1][i * 5] = ' ';
-            tablero[1][(i * 5 )+ 1] = ' ';
-            tablero[1][(i * 5 )+ 2] = '*';
-            tablero[1][(i * 5 )+ 3] = ' ';
-            tablero[1][(i * 5 )+ 4] = ' ';
+            tablero[1][(i * 5) + 1] = ' ';
+            tablero[1][(i * 5) + 2] = '*';
+            tablero[1][(i * 5) + 3] = ' ';
+            tablero[1][(i * 5) + 4] = ' ';
         }
     }
 }
 
-void mover_naves(char tablero[10][60]){
-    for (int i = 9; i > 0; i--)
+void mover_naves(char tablero[filas][columnas])
+{
+    for (int i = 1; i > 0; i--)
     {
         for (int j = 0; j < 60; j++)
         {
-            if(tablero[-1][j=='*'] || tablero [-1][j==' ']){
-                tablero[i][j] = tablero[i-1][j];
-                tablero[i-1][j]= ' ';
+            if (tablero[-1][j == '*'] || tablero[-1][j == ' '])
+            {
+                tablero[i][j] = tablero[i - 1][j];
+                tablero[i - 1][j] = ' ';
             }
+            printf("%c", tablero[i][j]);
         }
+        printf("\n");
     }
-    
 }
 
 int main()
 {
-    char tablero[10][60];
-
+    char tablero[filas][columnas];
     iniciar_tablero(tablero);
     iniciar_naves(tablero, 6);
-    for(int tick = 0; tick < 8; tick++){
-        imprimir_tablero(tablero);
+    imprimir_tablero(tablero);
+
+    for (int tick = 0; tick < 8; tick++)
+    {
         mover_naves(tablero);
-        _getch();
+        Sleep(1000);
     }
-    
+
     return 0;
 }
