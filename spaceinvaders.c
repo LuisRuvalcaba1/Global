3f017654a9051adaacabdbf336043ee48ad14259
Luis <a20100360@ceti.mx>
2024-08-23 12:38:28 -0600

    Borrar la fila superior (Spoiler no jalo)

--- spaceinvaders.c
+++ spaceinvaders.c
@@ -18,11 +18,15 @@ char iniciar_tablero(char tablero[filas][columnas])
     }
 }
 
-void imprimir_tablero(char tablero[filas][columnas])
+void imprimir_tablero(char tablero[filas][columnas], int fila)
 {
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < fila; i++)
     {
-        for (int j = 0; j < 60; j++)
+        printf("\n");
+    }
+    for (int i = 0; i < filas; i++)
+    {
+        for (int j = 0; j < columnas; j++)
         {
             printf("%c", tablero[i][j]);
         }
@@ -30,55 +34,58 @@ void imprimir_tablero(char tablero[filas][columnas])
     }
 }
 
-char iniciar_naves(char tablero[filas][columnas], int naves)
+char iniciar_naves(char tablero[filas][columnas])
 {
-    for (int i = 0; i < naves; i++)
+    for (int i = 0; i < 6; i++)
     {
-        for (int j = 0; j < 5; j++)
-        {
-            tablero[0][i * 5] = ' ';
-            tablero[0][(i * 5) + 1] = '*';
-            tablero[0][(i * 5) + 2] = ' ';
-            tablero[0][(i * 5) + 3] = '*';
-            tablero[0][(i * 5) + 4] = ' ';
+        tablero[0][i * 5] = ' ';
+        tablero[0][(i * 5) + 1] = '*';
+        tablero[0][(i * 5) + 2] = ' ';
+        tablero[0][(i * 5) + 3] = '*';
+        tablero[0][(i * 5) + 4] = ' ';
 
-            tablero[1][i * 5] = ' ';
-            tablero[1][(i * 5) + 1] = ' ';
-            tablero[1][(i * 5) + 2] = '*';
-            tablero[1][(i * 5) + 3] = ' ';
-            tablero[1][(i * 5) + 4] = ' ';
-        }
+        tablero[1][i * 5] = ' ';
+        tablero[1][(i * 5) + 1] = ' ';
+        tablero[1][(i * 5) + 2] = '*';
+        tablero[1][(i * 5) + 3] = ' ';
+        tablero[1][(i * 5) + 4] = ' ';
     }
 }
 
-void mover_naves(char tablero[filas][columnas])
-{
-    for (int i = 1; i > 0; i--)
-    {
-        for (int j = 0; j < 60; j++)
-        {
-            if (tablero[-1][j == '*'] || tablero[-1][j == ' '])
-            {
-                tablero[i][j] = tablero[i - 1][j];
-                tablero[i - 1][j] = ' ';
-            }
-            printf("%c", tablero[i][j]);
-        }
-        printf("\n");
-    }
-}
+// void mover_naves(char tablero[filas][columnas])
+// {
+//     for (int i = 1; i > 0; i--)
+//     {
+//         for (int j = 0; j < 60; j++)
+//         {
+//             if (tablero[-1][j == '*'] || tablero[-1][j == ' '])
+//             {
+//                 tablero[i][j] = tablero[i - 1][j];
+//                 tablero[i - 1][j] = ' ';
+//             }
+//         }
+//     }
+// }
 
 int main()
 {
     char tablero[filas][columnas];
+    int fila = 0;
     iniciar_tablero(tablero);
-    iniciar_naves(tablero, 6);
-    imprimir_tablero(tablero);
+    iniciar_naves(tablero);
+    // imprimir_tablero(tablero);
 
-    for (int tick = 0; tick < 8; tick++)
+    for (int tick = 0; tick < filas; tick++)
     {
-        mover_naves(tablero);
-        Sleep(1000);
+        system("cls");
+        imprimir_tablero(tablero, fila);
+        Sleep(500);
+        fila--;
+
+        if (fila < 0)
+        {
+            fila = filas - 2; 
+        }
     }
 
     return 0;
