b44d58fe2186511f1862869141256080fc7df9b1
Luis <a20100360@ceti.mx>
2024-08-23 08:55:59 -0600

    Baja pero mal

--- spaceinvaders.c
+++ spaceinvaders.c
@@ -32,18 +32,32 @@ char iniciar_naves(char tablero[10][60], int naves)
         for (int j = 0; j < 5; j++)
         {
             tablero[0][i * 5] = ' ';
-            tablero[0][i * 5 + 1] = '*';
-            tablero[0][i * 5 + 2] = ' ';
-            tablero[0][i * 5 + 3] = '*';
-            tablero[0][i * 5 + 4] = ' ';
+            tablero[0][(i * 5 )+ 1] = '*';
+            tablero[0][(i * 5 )+ 2] = ' ';
+            tablero[0][(i * 5 )+ 3] = '*';
+            tablero[0][(i * 5 )+ 4] = ' ';
 
             tablero[1][i * 5] = ' ';
-            tablero[1][i * 5 + 1] = ' ';
-            tablero[1][i * 5 + 2] = '*';
-            tablero[1][i * 5 + 3] = ' ';
-            tablero[1][i * 5 + 4] = ' ';
+            tablero[1][(i * 5 )+ 1] = ' ';
+            tablero[1][(i * 5 )+ 2] = '*';
+            tablero[1][(i * 5 )+ 3] = ' ';
+            tablero[1][(i * 5 )+ 4] = ' ';
         }
     }
+}
+
+void mover_naves(char tablero[10][60]){
+    for (int i = 9; i > 0; i--)
+    {
+        for (int j = 0; j < 60; j++)
+        {
+            if(tablero[-1][j=='*'] || tablero [-1][j==' ']){
+                tablero[i][j] = tablero[i-1][j];
+                tablero[i-1][j]= ' ';
+            }
+        }
+    }
+    
 }
 
 int main()
@@ -52,7 +66,11 @@ int main()
 
     iniciar_tablero(tablero);
     iniciar_naves(tablero, 6);
-    imprimir_tablero(tablero);
-
+    for(int tick = 0; tick < 8; tick++){
+        imprimir_tablero(tablero);
+        mover_naves(tablero);
+        _getch();
+    }
+    
     return 0;
 }
